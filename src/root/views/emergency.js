e53dd9cd301d339eb95100bb50063586609b455a
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-09-08 14:15:41 +0200

    Only 'Active' status Appeals are summed

--- src/root/views/emergency.js
+++ src/root/views/emergency.js
@@ -357,9 +357,11 @@ class Emergency extends React.Component {
         return selected ? o.id === selected : true;
       })
       .reduce((acc, o) => {
-        acc.beneficiaries += _toNumber(o.num_beneficiaries);
-        acc.funded += _toNumber(o.amount_funded);
-        acc.requested += _toNumber(o.amount_requested);
+        if (o.status === 0) {
+          acc.beneficiaries += _toNumber(o.num_beneficiaries);
+          acc.funded += _toNumber(o.amount_funded);
+          acc.requested += _toNumber(o.amount_requested);
+        }
         return acc;
       }, stats);
     const displayHeadlineStats =
